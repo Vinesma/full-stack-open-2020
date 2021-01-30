2c7a5eadef3e8958601c06aa6d7efa8bc7af3efd
Vinesma <otaviocos_13@hotmail.com>
2021-01-30 19:58:10 -0300

    Add exercises up to 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => {
@@ -21,6 +22,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndPoint);
 
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,18 @@
 const blogRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const {request, response} = require('express');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7);
+    }
+
+    return null;
+};
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 });
@@ -13,7 +24,16 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
     const body = request.body;
-    const user = await User.findOne({});
+    const token = getTokenFrom(request);
+    if (!token) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
     const blog = new Blog({
         title: body.title,
         author: body.author,
